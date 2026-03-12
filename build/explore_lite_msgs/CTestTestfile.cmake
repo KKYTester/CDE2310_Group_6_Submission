5a6f604d8c8955ee36c5715d4cca9ff83c5fd40d
KKYTester <kang123ky@gmail.com>
2026-03-12 17:29:17 +0800

    Initial commit

---
+++ build/explore_lite_msgs/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kky/cde2310_ws/src/m-explore-ros2-main/explore_lite_msgs
+# Build directory: /home/kky/cde2310_ws/build/explore_lite_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("explore_lite_msgs__py")
