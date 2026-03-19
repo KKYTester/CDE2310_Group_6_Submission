44e711c955a69d116d55312ef53bef6b865d9035
KKYTester <kang123ky@gmail.com>
2026-03-19 20:14:01 +0800

    (update): Config files for slam_toolbox and nav2 moved into workspace

--- launch/frontier_launch.py
+++ launch/frontier_launch.py
@@ -12,6 +12,8 @@ def generate_launch_description():
     nav2_dir = get_package_share_directory('nav2_bringup')
     slam_dir = get_package_share_directory('slam_toolbox')
     explore_dir = get_package_share_directory('explore_lite')
+    nav2_params = './param_files/nav2_params.yaml'
+    slam_toolbox_params = './param_files/mapper_params_online_async.yaml'
 
     rviz_config = os.path.join(
         nav2_dir,
@@ -19,7 +21,34 @@ def generate_launch_description():
         'nav2_default_view.rviz'
     )
 
-    # RViz
+    # Slam_Toolbox launch
+    slam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(slam_dir, 'launch', 'online_async_launch.py')
+        ),
+        launch_arguments={
+            'use_sim_time': 'false',
+            'params_file': slam_toolbox_params,
+        }.items()
+    )
+
+    # Nav2 bringup launch
+    nav2_launch = TimerAction(
+        period=5.0,  # seconds delay
+        actions=[
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(nav2_dir, 'launch', 'navigation_launch.py')
+                ),
+                launch_arguments={
+                    'use_sim_time': 'false',
+                    'params_file': nav2_params,
+                }.items()
+            )
+        ]
+    )
+
+    # RViz launch
     rviz_node = TimerAction(
         period=10.0,  # seconds delay
         actions=[
@@ -27,35 +56,14 @@ def generate_launch_description():
                 package='rviz2',
                 executable='rviz2',
                 name='rviz2',
+                parameters=[{'use_sim_time': False}],
                 arguments=['-d', rviz_config],
                 output='screen'
             )
         ]
     )
 
-    # Nav2 launch
-    nav2_launch = TimerAction(
-        period=5.0,  # seconds delay
-        actions=[
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(nav2_dir, 'launch', 'navigation_launch.py')
-                )
-            )
-        ]
-    )
-
-    # SLAM Toolbox
-    slam_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(slam_dir, 'launch', 'online_async_launch.py')
-        ),
-        launch_arguments={
-            'use_sim_time': 'false'
-        }.items()
-    )
-
-    # Frontier Exploration
+    # Frontier Exploration launch
     frontier_launch = TimerAction(
         period=15.0,  # seconds delay
         actions=[
@@ -67,12 +75,9 @@ def generate_launch_description():
         ]
     ) 
     
-    
-    
-
     return LaunchDescription([
-        nav2_launch,
         slam_launch,
+        nav2_launch,
         rviz_node,
         frontier_launch,
     ])
