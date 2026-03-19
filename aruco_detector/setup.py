1fef3f036c3928d12e98775a55efa3ab3c22fbb1
aksh-002 <akshbhatia002@gmail.com>
2026-03-19 20:28:40 +0800

    adding aruco files

---
+++ aruco_detector/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'aruco_detector'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/config', ['config/camera_calibration.yaml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ourpi',
+    maintainer_email='ourpi@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'aruco_node = aruco_detector.aruco_node:main',
+            'calibrate = aruco_detector.caliberation:main',
+        ],
+    },
+)
